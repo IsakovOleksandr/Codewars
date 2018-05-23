0e105538265e77f9540127727f357ff0d9f4d3e3
Oleksandr <ramfan_97@ukr.net>
2018-05-24 00:22:22 +0300

    Count character from string

---
+++ LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = Count("somehgdasdsssdaa");
+        }
+
+        public static Dictionary<char, int> Count(string str)
+        {
+            return str.GroupBy(i => i).ToDictionary(dict => dict.Key, dict => dict.Count());
+        }
+    }
+}
